af27b392b6e98a7a142646358a5519862e7341c6
Durval <jdccn@cin.ufpe.br>
2016-11-13 02:34:11 -0300

    Update pifi.sh

--- pifi.sh
+++ pifi.sh
@@ -9,18 +9,17 @@ fi
 
 
 echo "
-$(tput setaf 2)              .~~.   .~~.
-$(tput setaf 6)   /         $(tput setaf 2)'. \ ' ' / .'$(tput setaf 6)         \ 
-$(tput setaf 6)  |   /       $(tput setaf 1).~ .~~~..~.$(tput setaf 6)       \   |
-$(tput setaf 6) |   |   /  $(tput setaf 1) : .~.'~'.~. :$(tput setaf 6)   \   |   |
-$(tput setaf 6)|   |   |   $(tput setaf 1)~ (   ) (   ) ~$(tput setaf 6)   |   |   |
-$(tput setaf 6)|   |  |   $(tput setaf 1)( : '~'.~.'~' : )$(tput setaf 6)   |  |   |
-$(tput setaf 6)|   |   |   $(tput setaf 1)~ .~ (   ) ~. ~ $(tput setaf 6)  |   |   |
-$(tput setaf 6) |   |   \   $(tput setaf 1)(  : '~' :  )$(tput setaf 6)   /   |   |
-$(tput setaf 6)  |   \       $(tput setaf 1)'~ .~~~. ~'$(tput setaf 6)       /   |
-$(tput setaf 6)   \              $(tput setaf 1)'~'$(tput setaf 6)              / 
-$(tput bold ; tput setaf 4)            Raspberry PiFi$(tput sgr0)
-
+$(tput setaf 2)              .~~.   .~~.
+$(tput setaf 6)   /         $(tput setaf 2)'. \ ' ' / .'$(tput setaf 6)         \ 
+$(tput setaf 6)  |   /       $(tput setaf 1).~ .~~~..~.$(tput setaf 6)       \   |
+$(tput setaf 6) |   |   /  $(tput setaf 1) : .~.'~'.~. :$(tput setaf 6)   \   |   |
+$(tput setaf 6)|   |   |   $(tput setaf 1)~ (   ) (   ) ~$(tput setaf 6)   |   |   |
+$(tput setaf 6)|   |  |   $(tput setaf 1)( : '~'.~.'~' : )$(tput setaf 6)   |  |   |
+$(tput setaf 6)|   |   |   $(tput setaf 1)~ .~ (   ) ~. ~ $(tput setaf 6)  |   |   |
+$(tput setaf 6) |   |   \   $(tput setaf 1)(  : '~' :  )$(tput setaf 6)   /   |   |
+$(tput setaf 6)  |   \       $(tput setaf 1)'~ .~~~. ~'$(tput setaf 6)       /   |
+$(tput setaf 6)   \              $(tput setaf 1)'~'$(tput setaf 6)              / 
+$(tput bold ; tput setaf 4)            Raspberry PiFi$(tput sgr0)
 "
 
 echo "$(tput setaf 6)This script will configure your Raspberry Pi as a wireless access point.$(tput sgr0)"
@@ -34,7 +33,7 @@ echo "$(tput setaf 6)PiFi network SSID set to $(tput bold)$ssid$(tput sgr0 ; tpu
 pwd1="0"
 pwd2="1"
 until [ $pwd1 == $pwd2 ]; do
-  echo "$(tput bold ; tput setaf 2)Type a password to access your PiFi network, then press [ENTER]:$(tput sgr0)"
+  echo "$(tput bold ; tput setaf 2)Type a password(At least 8 characters) to access your PiFi network, then press [ENTER]:$(tput sgr0)"
   read -s pwd1
   echo "$(tput bold ; tput setaf 2)Verify password to access your PiFi network, then press [ENTER]:$(tput sgr0)"
   read -s pwd2
@@ -46,59 +45,98 @@ fi
 
  APPASS=$pwd1
  APSSID=$ssid
- 
- apt-get remove --purge hostapd -y
- apt-get install hostapd dnsmasq -y
- 
- cat > /etc/systemd/system/hostapd.service <<EOF
- [Unit]
- Description=Hostapd IEEE 802.11 Access Point
- After=sys-subsystem-net-devices-wlan0.device
- BindsTo=sys-subsystem-net-devices-wlan0.device
- [Service]
- Type=forking
- PIDFile=/var/run/hostapd.pid
- ExecStart=/usr/sbin/hostapd -B /etc/hostapd/hostapd.conf -P /var/run/hostapd.pid
- [Install]
- WantedBy=multi-user.target
- EOF
- 
- cat > /etc/dnsmasq.conf <<EOF
- interface=wlan0
- dhcp-range=10.0.0.2,10.0.0.5,255.255.255.0,12h
- EOF
- 
- cat > /etc/hostapd/hostapd.conf <<EOF
- interface=wlan0
- hw_mode=g
- channel=10
- auth_algs=1
- wpa=2
- wpa_key_mgmt=WPA-PSK
- wpa_pairwise=CCMP
- rsn_pairwise=CCMP
- wpa_passphrase=$APPASS
- ssid=$APSSID
- EOF
- 
- sed -i -- 's/allow-hotplug wlan0//g' /etc/network/interfaces
- sed -i -- 's/iface wlan0 inet manual//g' /etc/network/interfaces
- sed -i -- 's/    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf//g' /etc/network/interfaces
- 
- cat >> /etc/network/interfaces <<EOF
-     wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
- # Added by rPi Access Point Setup
- allow-hotplug wlan0
- iface wlan0 inet static
-     address 192.168.42.1
-     netmask 255.255.255.0
-     network 192.168.42.0
-     broadcast 192.168.42.255
- EOF
- 
- echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
- 
- systemctl enable hostapd
- 
- echo "All done! Please reboot"
- 
+if [[ $# -eq 2 ]]; then
+        APSSID=$2
+fi
+
+echo "$(tput setaf 6)Installing Necessarys Packages.$(tput sgr0)" 
+apt-get remove --purge hostapd -y
+apt-get install aptitude libnl-route-3-* -y
+aptitude install hostapd dnsmasq -y
+
+
+cat > /etc/systemd/system/hostapd.service <<EOF
+[Unit]
+Description=Hostapd IEEE 802.11 Access Point
+After=sys-subsystem-net-devices-wlan0.device
+BindsTo=sys-subsystem-net-devices-wlan0.device
+[Service]
+Type=forking
+PIDFile=/var/run/hostapd.pid
+ExecStart=/usr/sbin/hostapd -B /etc/hostapd/hostapd.conf -P /var/run/hostapd.pid
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/default/hostapd << EOF
+# Defaults for hostapd initscript
+#
+# See /usr/share/doc/hostapd/README.Debian for information about alternative
+# methods of managing hostapd.
+#
+# Uncomment and set DAEMON_CONF to the absolute path of a hostapd configuration
+# file and hostapd will be started during system boot. An example configuration
+# file can be found at /usr/share/doc/hostapd/examples/hostapd.conf.gz
+#
+DAEMON_CONF="/etc/hostapd/hostapd.conf"
+# Additional daemon options to be appended to hostapd command:-
+#       -d   show more debug messages (-dd for even more)
+#       -K   include key data in debug messages
+#       -t   include timestamps in some debug messages
+#
+# Note that -B (daemon mode) and -P (pidfile) options are automatically
+# configured by the init.d script and must not be added to DAEMON_OPTS.
+#
+#DAEMON_OPTS=""
+EOF
+echo "$(tput setaf 6)DNSAmasq configurated.$(tput sgr0)"
+cat > /etc/dnsmasq.conf <<EOF
+interface=wlan0      # Use interface wlan0  
+listen-address=10.0.0.1 # Explicitly specify the address to listen on  
+bind-interfaces      # Bind to the interface to make sure we aren't sending things elsewhere  
+server=8.8.8.8       # Forward DNS requests to Google DNS  
+domain-needed        # Don't forward short names  
+bogus-priv           # Never forward addresses in the non-routed address spaces.  
+dhcp-range=10.0.0.50,10.0.0.150,12h # Assign IP addresses between 172.24.1.50 and 172.24.1.150 with a 12 hour lease time  
+EOF
+
+echo "$(tput setaf 6)Hostapad Configurated.$(tput sgr0)"
+cat > /etc/hostapd/hostapd.conf <<EOF
+interface=wlan0
+hw_mode=g
+channel=10
+auth_algs=1
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_pairwise=CCMP
+rsn_pairwise=CCMP
+wpa_passphrase=$APPASS
+ssid=$APSSID
+EOF
+
+sed -i -- 's/allow-hotplug wlan0//g' /etc/network/interfaces
+sed -i -- 's/iface wlan0 inet manual//g' /etc/network/interfaces
+sed -i -- 's/    wpa-conf \/etc\/wpa_supplicant\/wpa_supplicant.conf//g' /etc/network/interfaces
+
+cat >> /etc/network/interfaces <<EOF
+        #wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+# Added by rPi Access Point Setup
+allow-hotplug wlan0
+iface wlan0 inet static
+        address 10.0.0.1
+        netmask 255.255.255.0
+        network 10.0.0.0
+        broadcast 10.0.0.255
+        wireless-mode Master
+
+EOF
+echo "$(tput setaf 6)Configuring your wlan0 interface with a static IP.$(tput sgr0)"
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE  
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT  
+
+systemctl enable hostapd
+
+echo "All done! Please reboot"
